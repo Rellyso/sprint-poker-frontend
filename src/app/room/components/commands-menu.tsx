c6ab1f294593b724f4c844635c1cc42ee57f5dc6
Rellyson <rellysondouglas2015@gmail.com>
2024-11-03 15:18:29 -0300

    feat: add commands menu

---
+++ src/app/room/components/commands-menu.tsx
@@ -0,0 +1,62 @@
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Check, ChevronDown, Copy } from "lucide-react";
+import { useState } from "react";
+
+enum GameType {
+  fibonacci = 'Fibonacci',
+  decimal = 'Decimal'
+}
+
+export function CommandsMenu() {
+  const [copied, setCopied] = useState(false)
+  const [selectedGameType, setSelectedGameType] = useState<GameType>(GameType.fibonacci)
+  const gameId = "11c1371a035"
+
+  const handleChangeGameType = (gameType: GameType) => {
+    setSelectedGameType(gameType)
+  }
+
+  const copyToClipboard = async () => {
+    await navigator.clipboard.writeText(gameId)
+    setCopied(true)
+    setTimeout(() => setCopied(false), 2000)
+  }
+
+  return (
+    <div className="mb-8 flex items-center gap-4">
+      <Button>Revelar</Button>
+      <Button variant="secondary">Resetar</Button>
+      <Check className="h-5 w-5 text-green-500" />
+
+      <div className="ml-auto flex items-center gap-4">
+        <Button
+          variant="outline"
+          className="gap-2"
+          onClick={copyToClipboard}
+        >
+          <Copy className={copied ? "hidden" : "h-4 w-4"} />
+          <Check className={copied ? "h-4 w-4" : "hidden"} />
+          {gameId}
+        </Button>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="gap-2">
+              {selectedGameType}
+              <ChevronDown className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end" className="w-[200px]">
+            <DropdownMenuItem onClick={() => handleChangeGameType(GameType.fibonacci)} className="text-blue-500">
+              Fibonacci
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => handleChangeGameType(GameType.decimal)} className="text-blue-500">
+              Decimal
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  )
+}
--- src/app/room/page.tsx
+++ src/app/room/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components/header";
+import { CommandsMenu } from "./components/commands-menu";
 
 export function RoomPage() {
+
   return (
     <div className="flex flex-col h-screen bg-background">
       <Header />
-      <div className="flex-1">
-        oi
+      <div className="container mx-auto px-4 pt-6">
+        <CommandsMenu />
       </div>
     </div>
   )
