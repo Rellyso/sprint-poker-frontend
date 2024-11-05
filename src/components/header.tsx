e27b5c0dd5b21a709c0219df5fc693358d8a6a07
Rellyson <rellysondouglas2015@gmail.com>
2024-11-05 20:26:15 -0300

    wip: add signout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,21 @@ import { ChevronDown, Sun } from "lucide-react";
 import { Button } from "./ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
 import { Avatar, AvatarFallback } from "./ui/avatar";
+import { useAuth } from "@/hooks/use-auth";
+import { useNavigate } from "react-router-dom";
 
 export function Header() {
+  const navigate = useNavigate()
+  const { session, signOut } = useAuth()
+
+  const userName = session?.user.name
+  const avatarFallback = session?.user.name[0].toLocaleUpperCase()
+
+  const handleSignOut = () => {
+    signOut()
+    navigate('/login')
+  }
+
   return (
     <header className="border-b">
       <div className="container flex h-16 items-center justify-between px-4 mx-auto">
@@ -21,15 +34,15 @@ export function Header() {
               <Button variant="ghost" className="gap-2">
                 <Avatar className="h-6 w-6">
                   {/* <AvatarImage src="/placeholder.svg" alt="User" /> */}
-                  <AvatarFallback>R</AvatarFallback>
+                  <AvatarFallback>{avatarFallback}</AvatarFallback>
                 </Avatar>
-                Rellyson
+                {userName}
                 <ChevronDown className="h-4 w-4" />
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
               <DropdownMenuItem>
-                <Button variant="destructive" className="w-full">
+                <Button variant="destructive" className="w-full" onClick={handleSignOut}>
                   Sair
                 </Button>
               </DropdownMenuItem>
